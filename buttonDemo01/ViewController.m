79816c6fef70ad364b59d77d498a97b392ade8f2
Andy <andy@AndydeMacBook-Air.local>
2017-06-20 17:24:32 +0800

    add move btn logic

--- buttonDemo01/ViewController.m
+++ buttonDemo01/ViewController.m
@@ -10,6 +10,15 @@
 
 @interface ViewController ()
 
+- (IBAction)up;
+
+- (IBAction)down;
+
+- (IBAction)right;
+
+- (IBAction)left;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnIcon;
 @end
 
 @implementation ViewController
@@ -24,4 +33,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)up {
+    // 获取按钮的y坐标的值，让y的值递减，然后再把新的值赋给按钮的y坐标
+    // 获取头像按钮坐标
+    // 获取按钮原始的frame值(这个frame中就包含来按钮的大小和坐标)
+    CGRect originFrame = self.btnIcon.frame;
+    // 设置y值-10
+    originFrame.origin.y -= 10;
+    
+    // 把新的frame再赋值给按钮
+    self.btnIcon.frame = originFrame;
+    
+    NSLog(@"up");
+}
+
+- (IBAction)down {
+    CGRect originFrame = self.btnIcon.frame;
+    originFrame.origin.y += 10;
+    
+    self.btnIcon.frame = originFrame;
+    NSLog(@"down");
+}
+
+- (IBAction)right {
+    CGRect originFrame = self.btnIcon.frame;
+    originFrame.origin.x += 10;
+    
+    self.btnIcon.frame = originFrame;
+    NSLog(@"right");
+}
+
+- (IBAction)left {
+    CGRect originFrame = self.btnIcon.frame;
+    originFrame.origin.x -= 10;
+    
+    self.btnIcon.frame = originFrame;
+    NSLog(@"left");
+}
 @end
